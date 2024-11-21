cfad83aa873992ee673874a9a48d8d9aefd1adc0
Giovani Nunez <nuezlogi@dukes.jmu.edu>
2024-11-20 22:17:36 -0500

    Part 2 done

--- js/part2.js
+++ js/part2.js
@@ -7,4 +7,25 @@ document.addEventListener("DOMContentLoaded", async () => {
     // The format should follow the example user in the HTML file.
 
     // TODO
+
+    let res = await fetch('https://reqres.in/api/users');
+    let users = (await res.json()).data;
+
+    const userList = document.getElementById('user-list');
+
+    users.forEach(user => {
+        let name = user.first_name + ' ' + user.last_name;
+
+        let div = document.createElement('div');
+        div.className = 'card';
+        div.innerHTML = '<h2>' + name + '</h2>'
+
+        let img = document.createElement('img');
+        img.src = user.avatar;
+        img.alt = name;
+        div.appendChild(img);
+
+        userList.appendChild(div);
+
+    });
 });
