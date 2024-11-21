38e1832a4c0d4f718d8ffce090abc9c6b6c99e90
Giovani Nunez <nuezlogi@dukes.jmu.edu>
2024-11-20 21:36:28 -0500

    Part 1 done

--- js/part1.js
+++ js/part1.js
@@ -5,6 +5,17 @@ document.getElementById("get-btn").addEventListener("click", async () => {
     // The two input fields should be included in the request URL as **query parameters**
 
     // TODO
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+
+    let q = new URLSearchParams({name, age})
+    let response = await fetch('https://echo.zuplo.io/api?' + q.toString());
+
+    let data = await response.json();
+
+    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+    
+
 });
 
 document.getElementById("post-json-btn").addEventListener("click", async () => {
@@ -12,6 +23,21 @@ document.getElementById("post-json-btn").addEventListener("click", async () => {
     // The two input fields should be included in the request body as **JSON data**
 
     // TODO
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+
+    let options = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify( {name, age} ),
+    };
+    let response = await fetch('https://echo.zuplo.io/api', options)
+
+    let data = await response.json();
+
+    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
 });
 
 document.getElementById("post-form-btn").addEventListener("click", async () => {
@@ -19,4 +45,20 @@ document.getElementById("post-form-btn").addEventListener("click", async () => {
     // The two input fields should be included in the request body as **url-encoded data**
 
     // TODO
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams( {name, age} ),
+    };
+    let response = await fetch('https://echo.zuplo.io/api', options);
+
+    let data = await response.json();
+
+    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+    
 });
